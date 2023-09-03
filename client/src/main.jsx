05fc77fc6cde8a9600c5321644629d4429bb8504
JonasKaruzas <jonas.karuzas@gmail.com>
2023-09-03 11:24:00 +0300

    feat: :lipstick: installs Material Design

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <CssBaseline>
     <App />
+  </CssBaseline>
 )
